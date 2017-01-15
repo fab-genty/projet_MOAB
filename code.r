2b773b2ddb4dae3304b3969a1efbfbfacf6bf8fd
fab-genty <electronsource75@gmail.com>
2017-01-15 18:36:52 +0100

    Add files via upload
    avec le nnet sur une seul ligne

--- code.r
+++ code.r
@@ -12,6 +12,8 @@ db=read.table(inputfile,sep="\t",header=T,colClasses = "character")
 #db est considéré comme un data_frame de "charactere", en tenir compte
 #opérations numeriques non possible sans conversion
 
+
+summary(db)
 #Realisation Echantillon APP/TEST
 echtApp=sample(1:nrow(db),2/3*nrow(db),replace=F)
 echtTest=(1:nrow(db))[-echtApp]
@@ -19,3 +21,11 @@ dbApp=db[echtApp,]
 dbTest=db[echtTest,]
 
 #Realisation du nnet
+
+
+
+v=as.vector(as.numeric(db[1,]$V1))
+t=as.vector(as.numeric(db[2,]$V2))
+
+library(nnet)
+nnet(v,t,size=4)
